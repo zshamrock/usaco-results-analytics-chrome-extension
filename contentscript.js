892593ae0ead80e35bc740d1a55c56bec2c48994
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2014-02-08 14:38:18 +0100

    Wrap contentscript into the closure.

--- contentscript.js
+++ contentscript.js
@@ -1,38 +1,39 @@
-"use strict";
+(function() {
+    "use strict";
 
-function collectData(request, sender, callback) {
-    console.log("Collecting the data...");
-    // TODO: use request and sender
+    function collectData(request, sender, callback) {
+        console.log("Collecting the data...");
+        // TODO: use request and sender
 
-    // how can we verify the sender of the message (it is tab, so it has the id)
-    var rows = $("tr", $("table").first()).slice(1),
-        i,
-        columns,
-        participants = [],
-        participant;
+        // how can we verify the sender of the message (it is tab, so it has the id)
+        var rows = $("tr", $("table").first()).slice(1),
+            i,
+            columns,
+            participants = [],
+            participant;
 
-    for (i  = 0; i < rows.length; i++) {
-        columns = $("td", rows.get(i));
-        participant = {country: columns.eq(0).text(),
-                       year: parseInt(jQuery.trim(columns.eq(1).text())),
-                       name: columns.eq(2).text(),
-                       score: parseInt(columns.eq(3).text())};
+        for (i  = 0; i < rows.length; i++) {
+            columns = $("td", rows.get(i));
+            participant = {country: columns.eq(0).text(),
+                           year: parseInt(jQuery.trim(columns.eq(1).text())),
+                           name: columns.eq(2).text(),
+                           score: parseInt(columns.eq(3).text())};
 
-        participants.push(participant);
+            participants.push(participant);
+        }
+
+        callback(JSON.stringify(participants));
     }
 
-    callback(JSON.stringify(participants));
-}
+    // activate the extension after the page content was completely loaded
+    $(document).ready(function() {
+        // Sends a single message to onMessage event listeners within the extension (or another extension/app).
+        // send message to the background script/page (background.js)
+        chrome.runtime.sendMessage({}); // enable show tab icon
 
-// activate the extension after the page content was completely loaded
-$(document).ready(function() {
-    // Sends a single message to onMessage event listeners within the extension (or another extension/app).
-    // send message to the background script/page (background.js)
-    chrome.runtime.sendMessage({}); // enable show tab icon
+        // message is sent from analytics.js when popup is open
+        chrome.runtime.onMessage.addListener(collectData);
+    });
 
-    // message is sent from analytics.js when popup is open
-    chrome.runtime.onMessage.addListener(collectData);
-});
-
-
+})();
 
